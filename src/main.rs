007b03f8455e3a349f07c27515619da7f2894611
Divkov575 <dmitriy@ivkov.net>
2024-05-20 22:45:09 -0400

    huh

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,48 @@ use polars::export::num::{pow, Pow};
 use util::{
     load_data,
     Weights,
+    Stack,
     constants::{
         MAX_ITERATIONS as MI
     }
 };
 
-
-
-
+// https://en.wikipedia.org/wiki/Cross-entropy#Cross-entropy_loss_function_and_logistic_regression
+// https://medium.com/@koushikkushal95/logistic-regression-from-scratch-dfb8527a4226
 fn main() -> Result<()> {
     let df = load_data();
     let mut weights = Weights::opt(&df);
+    let mut stack = Stack::empty();
+
+    stack.push(weights.clone(), mse(&df, &weights));
+    weights.m += 0.1;
+    stack.push(weights.clone(), mse(&df, &weights));
+
+    if stack.change2() == 0f64 {
+        println!("erm");
+    } else if stack.change2() < 0f64 {
+        weights.m += 0.1
+    } else if stack.change2() > 0f64 {
+        weights.m -= 0.1
+    }
 
     for i in 0..MI {
 
+        stack.push(weights.clone(), mse(&df, &weights));
 
+        if stack.change2() == 0f64 {
+            println!("erm");
+        } else if stack.change2() < 0f64 {
+            weights.m += 0.1
+        } else if stack.change2() > 0f64 {
+            weights.m -= 0.1
+        }
+
+
+
+    println!("{:?}", weights);
     }
+
 
 
 
@@ -39,10 +65,10 @@ fn logistic(x: f64, w: &Weights ) -> f64 {
     m / (1f64 + a * E.powf((-x)*b))
 }
 
-fn mse(df: DataFrame, weights: &Weights) -> f64 {
-    df.column("x")?
-        .f64()?
+fn mse(df: &DataFrame, weights: &Weights) -> f64 {
+    df.column("x").unwrap()
+        .f64().unwrap()
         .apply(|x| Some((x? - logistic(x?, &weights)).pow(2)))
         .into_series()
-        .sum()? / df.height()
+        .sum::<f64>().unwrap() / df.height() as f64
 }
--- src/util.rs
+++ src/util.rs
@@ -4,23 +4,25 @@ use polars::prelude::*;
 
 
 pub mod constants {
-    pub const MAX_ITERATIONS: usize = 10000;
+    pub const MAX_ITERATIONS: usize = 100;
 }
 
 pub fn load_data() -> DataFrame {
-    let file = File::open("inputs.csv").expect("could not open file");
+    // let file = File::open("inputs.csv").expect("could not open file");
+    let file = File::open("25:(1+e^-x).csv").expect("could not open file");
     CsvReader::new(file)
         .with_dtypes(Some(Arc::new(Schema::from_iter(vec![
             Field::new("x", DataType::Float64),
             Field::new("y", DataType::Float64)
         ]))))
         .has_header(true)
-        .finish()?
+        .finish()
+        .unwrap()
 
 }
 
 
-#[derive(Clone)]
+#[derive(Debug, Clone)]
 pub struct Weights {
     pub m: f64, // population max
     pub a: f64, //
@@ -29,11 +31,52 @@ pub struct Weights {
 
 impl Weights {
     pub fn empty() -> Self {
-        Self { m: 25f64, a: 1f64, b: 1f64 }
+        Self { m: 10f64, a: 1f64, b: 1f64 }
     }
     pub fn opt(df: &DataFrame) -> Self {
         let mut weights = Self::empty();
-        weights.m = df.column("y")?.max()?.unwrap();
+        weights.m = df.column("y").unwrap().max().unwrap().unwrap();
         weights
+    }
+}
+
+#[derive(Clone)]
+struct Pair {
+    weights: Weights,
+    score: f64,
+}
+
+pub struct Stack {
+    data: [Pair; 3]
+}
+impl Stack {
+    pub fn empty() -> Self {
+        Self {
+            data: [
+                Pair {
+                    weights: Weights::empty(),
+                    score: f64::MAX,
+                },
+                Pair {
+                    weights: Weights::empty(),
+                    score: f64::MAX,
+                },
+                Pair {
+                    weights: Weights::empty(),
+                    score: f64::MAX,
+                },
+            ],
+        }
+    }
+    pub fn change1(&self) -> f64 {
+        self.data[0].score - self.data[1].score
+    }
+    pub fn change2(&self) -> f64 {
+        self.data[1].score - self.data[2].score
+    }
+    pub fn push(&mut self, weights: Weights, score: f64) {
+        self.data[0] = self.data[1].clone();
+        self.data[1] = self.data[2].clone();
+        self.data[2] = Pair { weights, score };
     }
 }
