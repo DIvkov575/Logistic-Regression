12bf2ba260586f0eee16004aca12de784f3caabf
Divkov575 <dmitriy@ivkov.net>
2024-05-23 19:45:53 -0400

    converted to OO & working M-weight optimization

--- src/main.rs
+++ src/main.rs
@@ -3,51 +3,19 @@ mod util;
 use anyhow::Result;
 use polars::prelude::*;
 use std::f64::consts::E;
+use std::process::exit;
 use polars::export::num::{pow, Pow};
 use util::{
     load_data,
-    Weights,
-    Stack,
-    constants::{
-        MAX_ITERATIONS as MI
-    }
 };
 
 // https://en.wikipedia.org/wiki/Cross-entropy#Cross-entropy_loss_function_and_logistic_regression
 // https://medium.com/@koushikkushal95/logistic-regression-from-scratch-dfb8527a4226
 fn main() -> Result<()> {
-    let df = load_data();
-    let mut weights = Weights::opt(&df);
-    let mut stack = Stack::empty();
+    let mut df = load_data();
+    let mut lr = LogisticRegression::new();
 
-    stack.push(weights.clone(), mse(&df, &weights));
-    weights.m += 0.1;
-    stack.push(weights.clone(), mse(&df, &weights));
-
-    if stack.change2() == 0f64 {
-        println!("erm");
-    } else if stack.change2() < 0f64 {
-        weights.m += 0.1
-    } else if stack.change2() > 0f64 {
-        weights.m -= 0.1
-    }
-
-    for i in 0..MI {
-
-        stack.push(weights.clone(), mse(&df, &weights));
-
-        if stack.change2() == 0f64 {
-            println!("erm");
-        } else if stack.change2() < 0f64 {
-            weights.m += 0.1
-        } else if stack.change2() > 0f64 {
-            weights.m -= 0.1
-        }
-
-
-
-    println!("{:?}", weights);
-    }
+    lr.opt(&mut df);
 
 
 
@@ -57,18 +25,70 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn logistic(x: f64, w: &Weights ) -> f64 {
-    let m = w.m;
-    let a = w.a;
-    let b = w.b;
 
-    m / (1f64 + a * E.powf((-x)*b))
+#[derive(Debug, Clone)]
+pub struct LogisticRegression {
+    pub m: f64, // population max
+    pub a: f64, //
+    pub b: f64,
+    pub scores: Vec<f64>,
 }
 
-fn mse(df: &DataFrame, weights: &Weights) -> f64 {
-    df.column("x").unwrap()
-        .f64().unwrap()
-        .apply(|x| Some((x? - logistic(x?, &weights)).pow(2)))
-        .into_series()
-        .sum::<f64>().unwrap() / df.height() as f64
+impl LogisticRegression {
+    pub fn new() -> Self {
+        Self { m: 5f64, a: 1f64, b: 1f64, scores: vec![f64::MAX] }
+    }
+    fn opt(&mut self, df: &mut DataFrame) {
+        let mut sd: f64;
+        let mut delta = -1_f64;
+
+        self.scores.push(self.mse(&df));
+
+        for i in 0..25  {
+
+            self.m += delta;
+            self.scores.push(self.mse(&df));
+            sd = self.sd();
+
+            if sd < 0f64 {}
+            else if sd > 0f64 {
+                delta *= -1_f64
+            } else {
+                unimplemented!()
+            }
+        }
+
+    }
+    fn sd(&self) ->f64 {
+        self.scores[self.scores.len()-1] - self.scores[self.scores.len() - 2]
+    }
+    fn logistic(&self, x: f64) -> f64 {
+        let m = self.m;
+        let a = self.a;
+        let b = self.b;
+
+        m / (1f64 + a * E.powf((-x)*b))
+    }
+    fn _real_logistic(&self, x: f64) -> f64 {
+        let m = 25f64;
+        let a = 1f64;
+        let b = 1f64;
+
+        m / (1f64 + a * E.powf((-x)*b))
+    }
+    fn mse(&self, df: &DataFrame) -> f64 {
+        let fx_agg: f64 = df.column("x").unwrap()
+            .f64().unwrap()
+            .apply(|x| Some(self.logistic(x?)))
+            .sum().unwrap();
+        let y_agg: f64 = df.column("y").unwrap()
+            .sum().unwrap();
+
+        // println!("y_agg {}", y_agg);
+        // println!("fx_agg {}", fx_agg);
+        (y_agg.pow(2) - 2f64*y_agg*fx_agg + fx_agg.pow(2)) / (df.height() as f64)
+
+    }
 }
+
+
--- src/util.rs
+++ src/util.rs
@@ -3,9 +3,6 @@ use anyhow::Result;
 use polars::prelude::*;
 
 
-pub mod constants {
-    pub const MAX_ITERATIONS: usize = 100;
-}
 
 pub fn load_data() -> DataFrame {
     // let file = File::open("inputs.csv").expect("could not open file");
@@ -22,61 +19,3 @@ pub fn load_data() -> DataFrame {
 }
 
 
-#[derive(Debug, Clone)]
-pub struct Weights {
-    pub m: f64, // population max
-    pub a: f64, //
-    pub b: f64,
-}
-
-impl Weights {
-    pub fn empty() -> Self {
-        Self { m: 10f64, a: 1f64, b: 1f64 }
-    }
-    pub fn opt(df: &DataFrame) -> Self {
-        let mut weights = Self::empty();
-        weights.m = df.column("y").unwrap().max().unwrap().unwrap();
-        weights
-    }
-}
-
-#[derive(Clone)]
-struct Pair {
-    weights: Weights,
-    score: f64,
-}
-
-pub struct Stack {
-    data: [Pair; 3]
-}
-impl Stack {
-    pub fn empty() -> Self {
-        Self {
-            data: [
-                Pair {
-                    weights: Weights::empty(),
-                    score: f64::MAX,
-                },
-                Pair {
-                    weights: Weights::empty(),
-                    score: f64::MAX,
-                },
-                Pair {
-                    weights: Weights::empty(),
-                    score: f64::MAX,
-                },
-            ],
-        }
-    }
-    pub fn change1(&self) -> f64 {
-        self.data[0].score - self.data[1].score
-    }
-    pub fn change2(&self) -> f64 {
-        self.data[1].score - self.data[2].score
-    }
-    pub fn push(&mut self, weights: Weights, score: f64) {
-        self.data[0] = self.data[1].clone();
-        self.data[1] = self.data[2].clone();
-        self.data[2] = Pair { weights, score };
-    }
-}
