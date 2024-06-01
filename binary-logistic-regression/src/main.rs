0112baed1589ace1ee272a2f3441f6c1bd58edbb
Divkov575 <dmitriy@ivkov.net>
2024-06-01 14:29:18 -0400

    in between custom code -> will experimnet with ndarray

--- binary-logistic-regression/src/main.rs
+++ binary-logistic-regression/src/main.rs
@@ -1,4 +1,5 @@
 use std::f64;
+use ndarray::array;
 
 
 fn main() {
@@ -53,7 +54,17 @@ impl LR {
     pub fn _sigmoid(x: f64) -> f64 {
         1f64 / (1f64 + f64::consts::E.powf(-x))
     }
-    pub fn bce(&self, y_true: Vec<f64>, y_pred: Vec<f64>) -> f64 {
+
+    pub fn predict(&self, X: Vec<Vec<f64>>) -> Vec<f64> {
+        let output: Vec<f64> = Vec::new();
+        for (index, value) in dot_2x1(X, &self.weights).iter().enumerate() {
+            output[index] = Self::_sigmoid(value + self.bias);
+        }
+
+        output
+    }
+
+    pub fn loss(&self, y_true: Vec<f64>, y_pred: Vec<f64>) -> f64 {
         // binary cross entropy loss
         let mut y1 = 0f64;
         let mut y2 = 0f64;
@@ -66,50 +77,40 @@ impl LR {
         -(y1 + y2) / y_true.len() as f64
     }
 
-    pub fn feed_forward(&self, X: Vec<Vec<f64>>) {
-        let z = dot(X, transpose(Vec::from_iter([self.weights.clone()])));
-        let A = Self::sigmoid(z);
-    }
-
-    // pub fn fit(&mut self, X: &[&[f64]], y: &[f64]) {
-    //     let dw;
-    //     let db;
-    //     let n_samples = X.len();
-    //     let n_features = X[0].len();
-    //     self.weights = vec![0f64; n_features];
-    //     self.bias = 0f64;
-    //
-    //     for _ in 0..self.n_iters {
-    //         let A = self.feed_forward(X);
-    //         let dz = A - y; // derivative of sigmoid and bce X.T*(A-y)
-    //
-    //         dw = (1 / n_samples) * A.iter().zip(dz.iter()).map(|(x, y)| x * y).sum();
-    //         db = (1 / n_samples) * A.iter().zip(dz.iter()).map(|(x, y)| x - y).sum();
-    //
-    //         self.weights -= self.lr * dw;
-    //         self.bias -= self.lr * db;
-    //     }
-    // }
-
-    // pub fn predict(&self, X: &[&[f64]]) -> Vec<f64> {
-    //     let threshold = 0.5;
-    //     let y_hat = X.iter().zip(self.weights.iter()).map(|(x, y)| x * y).sum() + self.bias; // todo
-    //     // let y_
-    // }
+    pub fn gd(&self)
 }
 
 
+// #[allow(non_snake_case)]
+// fn dot_2x2(A: Vec<Vec<f64>>, B: Vec<Vec<f64>>) -> Vec<Vec<f64>> {
+//     assert_eq!(A[0].len(), B.len());
+//     let mut output: Vec<Vec<f64>> = vec![vec![0f64; B[0].len()]; A[0].len()];
+//
+//     let j = 0;
+//
+//     for h in 0..A.len() {
+//         for i in 0..A[0].len() {
+//             output[h][j] += A[h][i] * B[i][j];
+//         }
+//     }
+//
+//     for row in &output {
+//         println!("{:?}", row);
+//     }
+//
+//
+//     output
+// }
 
 #[allow(non_snake_case)]
-fn dot(A: Vec<Vec<f64>>, B: Vec<Vec<f64>>) -> Vec<Vec<f64>> {
+fn dot_2x1(A: Vec<Vec<f64>>, B: &[f64]) -> Vec<f64> {
     assert_eq!(A[0].len(), B.len());
-    let mut output: Vec<Vec<f64>> = vec![vec![0f64; B[0].len()]; A[0].len()];
-
-    let j = 0;
+    // let mut output: Vec<Vec<f64>> = vec![vec![0f64; B[0].len()]; A[0].len()];
+    let mut output = vec![0f64; A[0].len()];
 
     for h in 0..A.len() {
         for i in 0..A[0].len() {
-            output[h][j] += A[h][i] * B[i][j];
+            output[h] += A[h][i] * B[i];
         }
     }
 
