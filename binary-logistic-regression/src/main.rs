b3c37d602815760aa182f561861f38f48617e3ef
Divkov575 <dmitriy@ivkov.net>
2024-06-01 22:06:10 -0400

    reading data from csv

--- binary-logistic-regression/src/main.rs
+++ binary-logistic-regression/src/main.rs
@@ -1,15 +1,37 @@
 #![allow(non_snake_case)]
 mod logistic_regression;
 
+use std::fs::File;
 use ndarray::{array, Array1, Array2, Ix2, Shape};
 use logistic_regression::*;
 use csv;
 
 
 
-fn main() {
-    let X = array![-4.50,-4.00,-3.50,-3.00,-2.50,-2.00,-1.50,-1.00,-0.50,0.00,0.50,1.00,1.50,2.00,2.50,3.00,3.50,4.00,4.50].into_shape((19, 1)).unwrap();
-    let y: Array1<f64> = array![0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,1.,1.,1.,1.,1.,1.,1.,1.,1.,].into();
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let path = std::path::Path::new("data.csv");
+    let mut rdr = csv::ReaderBuilder::new()
+        .from_reader(File::options().read(true).open(path)?);
+
+    let mut x_vec = Vec::new();
+    let mut y_vec: Vec<f64> = Vec::new();
+
+    for record_result in rdr.records() {
+        let record: Vec<f64> = record_result?.iter().map(|x| x.replace(" ", "").parse::<f64>().unwrap()).collect();
+        x_vec.extend(record[0..record.len()-1].to_vec());
+        y_vec.push(record[record.len()-1]);
+    }
+
+    let num_records = y_vec.len();
+    let num_features = x_vec.len() / num_records;
+
+    let X: Array2<f64> = Array1::from_iter(x_vec).into_shape((num_records, num_features))?;
+    let y: Array1<f64> = Array1::from_iter(y_vec);
+
+
+
+
+
 
     let mut lr = LR::empty();
     lr.fit(X.clone(), y);
@@ -20,7 +42,7 @@ fn main() {
 
 
 
-
+    Ok(())
 }
 
 
